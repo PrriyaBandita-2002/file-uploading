574ccf8d277aff91a7be26ad542f857858e22d3a
Prriya dash <119881879+PrriyaBandita-2002@users.noreply.github.com>
2025-07-27 23:17:40 +0530

    Update api.js

--- Client/src/service/api.js
+++ Client/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://file-uploading-okii.onrender.com";
 
 export const uploadFile = async (data) => {
   try {
